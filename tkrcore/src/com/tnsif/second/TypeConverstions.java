f9409509b3bef378650e700d9169b271ac9b0702
korramahesh45 <korramahesh03@gmail.com>
2025-09-16 22:34:42 +0530

    second commit

---
+++ tkrcore/src/com/tnsif/second/TypeConverstions.java
@@ -0,0 +1,23 @@
+package com.tnsif.second;
+
+public class TypeConverstions {
+
+	public static void main(String[] args) {
+		/*implicit conversion
+		 * int to float value conversion
+		 */
+	int a=30;
+	float b=a;
+	System.out.println(b);
+	/*
+	 * explicit conversion
+	 * float to int value conversion
+	 * 
+	 */
+	double c=20.5;
+	int d=(int)c;
+	System.out.println(d);
+
+	}
+
+}
