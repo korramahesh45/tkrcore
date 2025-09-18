42c76ccce1043735b3f8c83592d7a708cd7301f8
korramahesh45 <korramahesh03@gmail.com>
2025-09-18 09:36:57 +0530

    5 comiit

---
+++ tkrcore/src/com/tnsif/second/Palindrome.java
@@ -0,0 +1,34 @@
+package com.tnsif.second;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (isPalindrome(num)) {
+            System.out.println(num + " is a Palindrome");
+        } else {
+            System.out.println(num + " is NOT a Palindrome");
+        }
+
+        sc.close();
+    }
+
+    // Method to check palindrome
+    public static boolean isPalindrome(int num) {
+        int original = num;
+        int reversed = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+
+        return original == reversed;
+    }
+}
